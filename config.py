4d1c12f82be2f17becc5beba01941e90049f362e
jmamine <109294107+jmamine@users.noreply.github.com>
2024-07-13 10:04:54 +0100

    fix: configure settings with environment variables in config.py

--- config.py
+++ config.py
@@ -1,24 +1,26 @@
-# config.py
-
 import logging
 import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
     """
     Base configuration class.
     """
-    UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), 'scripts')
-    DEBUG = False
-    TESTING = False
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'default-secret-key')
-    PORT = int(os.environ.get('PORT', 5000))
+    UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER')
+    DEBUG =os.environ.get('DEBUG').lower() in ('true', '1')
+    TESTING = os.environ.get('TESTING').lower() in ('true', '1')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    PORT = int(os.environ.get('PORT'))
     # Add other configuration options as needed
 
 class DevelopmentConfig(Config):
     """
     Development configuration class.
     """
-    DEBUG = True
+    DEBUG = os.environ.get('DEBUG').lower() in ('true', '1')
     # Add development-specific configuration options if needed
 
 class ProductionConfig(Config):
@@ -26,14 +28,14 @@ class ProductionConfig(Config):
     Production configuration class.
     """
     # Add production-specific configuration options if needed
-    pass
+    
 
 class TestingConfig(Config):
     """
     Testing configuration class.
     """
-    TESTING = True
-    UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), 'test_scripts')
+    TESTING = os.environ.get('TESTING').lower() in ('true', '1')
+    UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER')
     # Add testing-specific configuration options if needed
 
 # Dictionary to hold the different configuration classes
@@ -50,4 +52,3 @@ def get_config(env):
     except Exception as e:
         logging.exception('An error occurred while getting configuration: %s', e)
         return Config()
-
